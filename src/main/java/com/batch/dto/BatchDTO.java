f76036680ae97322627a64019ad796b9d879db70
Sunil Jadhav <suniljadhav790@gmail.com>
2020-03-08 14:07:57 +0530

    Batch-Management : Initial Commit

---
+++ src/main/java/com/batch/dto/BatchDTO.java
@@ -0,0 +1,17 @@
+package com.batch.dto;
+
+import lombok.Data;
+
+@Data
+public class BatchDTO {
+
+	private Long id;
+	private String batchId;
+	private String name;
+	private String timing;
+	private String duration;
+	private String startedAt;
+	private String isActive;
+	private Double collection;
+	private Double pendingCollection;
+}
